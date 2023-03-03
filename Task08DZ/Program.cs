dc45b06fb1d55e132cd8b270e6f006f8609cb20e
Sergei <serfe121y@rambler.ru>
2023-03-03 20:04:37 +0300

    Task(08,02,04,06)

---
+++ Task08DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
