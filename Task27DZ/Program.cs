2a01f8f4533a961f8f4d47c2fe57c3dbeaf99e83
Sergei <serfe121y@rambler.ru>
2023-03-17 19:12:52 +0300

    Добавил задания 27...

---
+++ Task27DZ/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+Console.WriteLine("Введите  число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int summa = SumAllDigit(number);
+Console.WriteLine($"Сумма цифр в  {number} --> {summa} ");
+
+
+int SumAllDigit(int number)
+{
+  int result = 0;
+  while (number > 0)
+  {
+    result+=number % 10;
+    number = number/10;
+    
+  }
+  return result;
+}
+
---
+++ help arguments/Program.cs
@@ -0,0 +1,39 @@
+﻿// архив аргументов 
+
+
+// аргумент название collection сделали рандомные числа от 1 до 10
+void FillArray(int[] collection)
+{
+    // длинна массива .Length
+    int length = collection.Length;
+    //    по умолчанию позиция индекс начинаеться с 0
+    int index = 0;
+    while (index < length)
+    {
+        // обращение к аргументу (collection) на позицию индех и положить туда новое случайное число(целое число из диапозона 1,10)
+        collection[index] = new Random().Next(1, 10);
+       // index = index + 1;
+    index++;
+    }
+}
+
+// void не возвращает значения поэтому мы не используем retern
+void PrintArray(int[] col)
+{
+    // количество элементов
+int count = col.Length;
+int position = 0;
+while(position < count)
+{
+    Console.WriteLine(col[position]);
+    position++;
+}
+}
+
+// массив из 10 элементов
+int[] array = new int[10];
+
+// вызвали метод FillArray который заполнил элементы
+FillArray(array);
+// вызвали метод который распечатал элементы
+PrintArray(array);
